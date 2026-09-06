bbbc786ce7728c43a7e6e842478a5f7f81dd8309
ahmed-tarek <c.tarek2015@gmail.com>
2026-09-06 11:38:21 +0300

    add cors middleware to enable cross-origin requests

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const app = express();
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const petsCtrl = require("./controllers/petsCtrl.js");
+const cors = require("cors");
 
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-
+// app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 app.use(express.json());
 app.use(logger("dev"));
 
